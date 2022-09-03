05654cb3f7612d64f699b6f0c4103fa92b647a20
DMyrzich <myrzich@bk.ru>
2022-09-03 15:15:22 +0300

    Update ormconfig.ts

--- src/server/ormconfig.ts
+++ src/server/ormconfig.ts
@@ -2,18 +2,18 @@ import { ConnectionOptions } from "typeorm";
 
 const config: ConnectionOptions = {
     type: 'postgres',
-    host: 'ec2-44-197-40-76.compute-1.amazonaws.com',
-    port: 5432,
+    host: 'NONE',
+    port: NONE,
     ssl: true,
     extra: {
         ssl: {
             rejectUnauthorized: false
         },
     },
-    username: 'ixvmhqbaiqwkiw',
-    password: '198a915903aad44091c7c469f7635e019b4efcf2c903e24d6bdf570bc57a76c1',
-    database: 'd5pjd57fhmrb73',
-    url: 'postgres://ixvmhqbaiqwkiw:198a915903aad44091c7c469f7635e019b4efcf2c903e24d6bdf570bc57a76c1@ec2-44-197-40-76.compute-1.amazonaws.com:5432/d5pjd57fhmrb73',
+    username: 'NONE',
+    password: 'NONE',
+    database: 'NONE',
+    url: 'NONE',
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: false,
     logging: false,
